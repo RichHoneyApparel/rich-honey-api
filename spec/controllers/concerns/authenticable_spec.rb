05c5103955dc084678617af4bef292b01d500d49
Gamaliel Padillo <gamalielgad1234@gmail.com>
2017-02-07 11:28:41 -0800

    Test if user is signed in

--- spec/controllers/concerns/authenticable_spec.rb
+++ spec/controllers/concerns/authenticable_spec.rb
@@ -36,4 +36,23 @@ describe Authenticable do
       expect(authentication.authenticate_with_token![:status]).to eq :unauthorized
     end
   end
+
+  describe '#user_signed_in?' do
+    context 'when there is a user' do
+      before do
+        @user = FactoryGirl.create(:user)
+        authentication.stub(:current_user).and_return(@user)
+      end
+
+      it { should be_user_signed_in }
+    end
+
+    context 'when there is no user' do
+      before do
+        @user = FactoryGirl.create(:user)
+        authentication.stub(:current_user).and_return(nil)
+      end
+      it { should_not be_user_signed_in }
+    end
+  end
 end
