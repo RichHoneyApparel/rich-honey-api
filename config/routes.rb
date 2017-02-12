f583c8d343f89c6ca704cb4f5efa11b977f46632
Gamaliel Padillo <gamalielgad1234@gmail.com>
2017-02-11 18:54:18 -0800

    Remove api subodomain parameters

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ require 'api_constraints'
 
 RichHoneyApi::Application.routes.draw do
   devise_for :users, skip: :registrations
-  namespace :api, defaults: { format: :json },constraints: { subdomain: 'api' }, path: '/'  do
+  namespace :api, defaults: { format: :json }, path: '/' do
     scope module: :v1, constraints: ApiConstraints.new(version: 1, default: true) do
       resources :users, only: [:index, :show, :create, :update, :destroy]
       # destroy should be changed to archived. In the mean time, have it as destroy
